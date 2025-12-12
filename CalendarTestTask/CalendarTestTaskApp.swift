04b6b7acab8745e228e7fde4badb9dbb9485ddf6
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-12-12 10:48:18 +0300

    Initial Commit

---
+++ CalendarTestTask/CalendarTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarTestTaskApp.swift
+//  CalendarTestTask
+//
+//  Created by Andrey Efimov on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CalendarTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
