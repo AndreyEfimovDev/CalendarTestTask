183a6bf66181bd29f066df77b32faeec6da4feff
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-12-16 12:49:47 +0300

    Commenting preview wrapper

--- CalendarTestTask/PreviewHelpers/PreviewHelpers.swift
+++ CalendarTestTask/PreviewHelpers/PreviewHelpers.swift
@@ -5,47 +5,36 @@
 //  Created by Andrey Efimov on 12.12.2025.
 //
 
-import SwiftUI
-
-struct PreviewWrapper<Content: View>: View {
-    let content: () -> Content
-    
-    init(@ViewBuilder content: @escaping () -> Content) {
-        self.content = content
-    }
-    
-    var body: some View {
-        let apiService = MockDataService()
-        let coordinator = AppCoordinator(apiService: apiService)
-        
-        return NavigationStack {
-            content()
-                .environmentObject(coordinator)
-        }
-    }
-}
-
-#Preview("Calendar View") {
-    PreviewWrapper {
-        let apiService = MockDataService()
-        let coordinator = AppCoordinator(apiService: apiService)
-        let viewModel = CalendarViewModel(
-            apiService: apiService,
-            initialDate: coordinator.initialCalendarDate
-        )
-        
-        return CalendarView(viewModel: viewModel)
-    }
-}
+//import SwiftUI
 //
-//#Preview("Day Events View") {
-//    PreviewWrapper {
+//struct PreviewWrapper<Content: View>: View {
+//    let content: () -> Content
+//    
+//    init(@ViewBuilder content: @escaping () -> Content) {
+//        self.content = content
+//    }
+//    
+//    var body: some View {
 //        let apiService = MockDataService()
-//        let viewModel = DayEventsViewModel(
-//            date: Date(),
-//            apiService: apiService
-//        )
+//        let coordinator = AppCoordinator(apiService: apiService)
 //        
-//        return DayEventsView(viewModel: viewModel)
+//        return NavigationStack {
+//            content()
+//                .environmentObject(coordinator)
+//        }
 //    }
 //}
+//
+//#Preview("Calendar View") {
+//    PreviewWrapper {
+//        let apiService = MockDataService()
+//        let coordinator = AppCoordinator(apiService: apiService)
+//        let viewModel = CalendarViewModel(
+//            apiService: apiService,
+//            initialDate: coordinator.initialCalendarDate
+//        )
+//        
+//        return CalendarView(viewModel: viewModel)
+//    }
+//}
+//
